5d2980a5107fa7f142b4caad60e7aa840271bb56
Jason Rueckert <phlacin@gmail.com>
2024-09-02 01:44:43 -0700

    Changing to a helicoptor theme

--- player.gd
+++ player.gd
@@ -1,67 +1,73 @@
 extends RigidBody3D
 
-## How much vertical force to apply when moving.
 @export_range(750.0, 3500.0) var thrust := 1000.0
 @export var torque_thrust := 100.0
+@export var rotor_speed := 5.0
 
-@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
-@onready var success_audio: AudioStreamPlayer = $SuccessAudio
 @onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
 @onready var booster_particles: GPUParticles3D = $BoosterParticles
 @onready var right_booster_particles: GPUParticles3D = $RightBoosterParticles
 @onready var left_booster_particles: GPUParticles3D = $LeftBoosterParticles
-@onready var explosion_particles: GPUParticles3D = $ExplosionParticles
-@onready var success_particles: GPUParticles3D = $SuccessParticles
-@onready var rocket_body: Node3D = $RocketBody
-
+@onready var helicoptor_body: Node3D = $HelicoptorBody
+@onready var rotor: Node3D = $HelicoptorBody/Rotor
 
 var is_transitioning := false
+var rotors_active := false
+var initial_rotor_y_rotation: float
+var can_restart_rotor := true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	initial_rotor_y_rotation = rotor.rotation.y
+	print("Initial rotor Y rotation: ", initial_rotor_y_rotation)
+
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
+		if can_restart_rotor and not rotors_active:
+			start_rotor()
 		apply_central_force(basis.y * delta * thrust)
 		booster_particles.emitting = true
-		if !rocket_audio.playing:
-			rocket_audio.play()
 	else:
 		booster_particles.emitting = false
-		rocket_audio.stop()
-	
+
+	if rotors_active:
+		rotate_rotors(delta)
+
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
 		right_booster_particles.emitting = true
 	else:
 		right_booster_particles.emitting = false
-	
+
 	if Input.is_action_pressed("rotate_right"):
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 		left_booster_particles.emitting = true
 	else:
 		left_booster_particles.emitting = false
-		
-	
+
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
 
+func rotate_rotors(delta: float):
+	rotor.rotate_y(delta * rotor_speed)
+
+func stop_rotors(delta):
+	pass
 
 func _on_body_entered(body: Node) -> void:
 	if not is_transitioning:
 		if "Goal" in body.get_groups():
 			complete_level(body.file_path)
-			
 		if "Hazard" in body.get_groups():
 			crash_sequence()
-	
+		if "LaunchPad" in body.get_groups():
+			land()
+
 func crash_sequence():
 	print("YOU LOST IT!")
-	#rocket_body.set_deferred("visible", false)
-	rocket_body.visible = false
+	helicoptor_body.visible = false
 	right_booster_particles.visible = false
 	left_booster_particles.visible = false
 	booster_particles.visible = false
-	explosion_particles.emitting = true
-	explosion_audio.play()
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
@@ -70,12 +76,23 @@ func crash_sequence():
 
 func complete_level(next_level_file: String):
 	print("Level Complete")
-	success_particles.emitting = true
-	success_audio.play()
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
 	tween.tween_interval(1.5)
-	tween.tween_callback(
-		get_tree().change_scene_to_file.bind(next_level_file)
-	)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file))
+
+func land():
+	if rotor.rotation.y != 0:
+		var tween = create_tween()
+		tween.tween_property(self, "rotor_speed", 0, 1.0)
+		tween.tween_callback(func():
+			rotors_active = false
+			can_restart_rotor = true
+		)
+
+func start_rotor():
+	rotors_active = true
+	can_restart_rotor = false
+	var tween = create_tween()
+	tween.tween_property(self, "rotor_speed", 25.0, 0.5) 
