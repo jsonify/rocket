69905eae35364616458f1cd32e53e24a07b585d9
Jason Rueckert <phlacin@gmail.com>
2024-09-04 12:58:15 -0700

    adding camera zoom

--- player.gd
+++ player.gd
@@ -5,17 +5,27 @@ class_name Player extends RigidBody3D
 @export var rotor_speed := 5.0
 
 @onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
-@onready var booster_particles: GPUParticles3D = $BoosterParticles
-@onready var right_booster_particles: GPUParticles3D = $RightBoosterParticles
-@onready var left_booster_particles: GPUParticles3D = $LeftBoosterParticles
+
 @onready var helicoptor_body: Node3D = $HelicoptorBody
 @onready var rotor: Node3D = $HelicoptorBody/Rotor
 @onready var bullet_spawn: Marker3D = $HelicoptorBody/BulletSpawn
+@onready var camera_mount: Node3D = $CameraMount
+@onready var camera_3d: Camera3D = $CameraMount/Camera3D
 
 var is_transitioning := false
 var rotors_active := false
 var initial_rotor_y_rotation: float
 var can_restart_rotor := true
+
+# Camera zoom variables
+@export var zoom_in_fov := 60.0  # Adjust this value for closer zoom
+@export var zoom_out_fov := 90.0  # Adjust this value for wider zoom
+@export var zoom_speed := 2.0  # Adjust this value to change zoom speed
+var current_fov := zoom_in_fov
+
+# Camera position offset
+var camera_offset := Vector3(0, 2, 5)  # Adjust these values to position the camera
+var initial_camera_rotation: Basis
 
 var bullet_scene = preload("res://bullet.tscn")
 @export var fire_rate := 0.2
@@ -23,13 +33,31 @@ var can_fire := true
 
 func _ready():
 	initial_rotor_y_rotation = rotor.rotation.y
-	print("Initial rotor Y rotation: ", initial_rotor_y_rotation)
+	initial_camera_rotation = camera_3d.global_transform.basis
+	current_fov = camera_3d.fov
 
 func _process(delta: float) -> void:
 	if rotors_active:
 		rotate_rotors(delta)
 		
 	handle_input(delta)
+	handle_camera_zoom(delta)
+	update_camera_position()
+
+func update_camera_position():
+	# Calculate the camera position based on the helicopter's current orientation
+	var rotated_offset = global_transform.basis * camera_offset
+	var new_camera_position = global_position + rotated_offset
+
+	# Update the camera's global position
+	camera_3d.global_position = new_camera_position
+
+	# Lock the camera's rotation to its initial state
+	camera_3d.global_transform.basis = initial_camera_rotation
+
+	# Make the camera look at a point slightly above the helicopter
+	var look_target = global_position + global_transform.basis.y * 1.5
+	camera_3d.look_at(look_target, Vector3.UP)
 
 func handle_input(delta):
 	# thrust
@@ -37,9 +65,6 @@ func handle_input(delta):
 		if can_restart_rotor and not rotors_active:
 			start_rotor()
 		apply_central_force(basis.y * delta * thrust)
-		booster_particles.emitting = true
-	else:
-		booster_particles.emitting = false
 	
 	# reverse direction
 	if Input.is_action_just_pressed("reverse_direction"):
@@ -48,16 +73,12 @@ func handle_input(delta):
 	# backwards
 	if Input.is_action_pressed("backwards"):
 		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
-		right_booster_particles.emitting = true
-	else:
-		right_booster_particles.emitting = false
+
 
 	# forward
 	if Input.is_action_pressed("forward"):
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
-		left_booster_particles.emitting = true
-	else:
-		left_booster_particles.emitting = false
+
 
 	# fire bullet
 	if Input.is_action_pressed("fire") and can_fire:
@@ -105,9 +126,6 @@ func _on_body_entered(body: Node) -> void:
 func crash_sequence():
 	print("YOU LOST IT!")
 	helicoptor_body.visible = false
-	right_booster_particles.visible = false
-	left_booster_particles.visible = false
-	booster_particles.visible = false
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
@@ -136,3 +154,8 @@ func start_rotor():
 	can_restart_rotor = false
 	var tween = create_tween()
 	tween.tween_property(self, "rotor_speed", 25.0, 0.5) 
+
+func handle_camera_zoom(delta):
+	var target_fov = zoom_out_fov if Input.is_action_pressed("thrust") else zoom_in_fov
+	camera_3d.fov = move_toward(current_fov, target_fov, zoom_speed * delta)
+	camera_3d.fov = current_fov
